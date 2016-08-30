6316777b19a6ad5d7864a087884aca7f6585006a
Jake Coppinger <jake@jakecoppinger.com>
2016-08-30 13:55:55 +1000

    Working face tracking

--- face.py
+++ face.py
@@ -1,25 +1,33 @@
-import numpy as np
 import cv2
- 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+import sys
 
-print(face_cascade)
+# Get user supplied values
+imagePath = "abba.png" #sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml" #sys.argv[2]
 
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-img = cv2.imread('snap.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+faces = non_max_suppression(faces, probs=None, overlapThresh=0.65)
 
-cv2.imshow('img',img)
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
