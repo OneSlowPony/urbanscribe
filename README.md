dc2698e29bc149476471234a90a865d525bfc74b
Jake Coppinger <jake@jakecoppinger.com>
2016-08-30 15:57:53 +1000

    Current work

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # barracklane
+
+
+# Notes
+https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
+https://realpython.com/blog/python/face-recognition-with-python/
+http://www.pyimagesearch.com/tag/pedestrian-detection/
+http://www.pyimagesearch.com/2015/11/16/hog-detectmultiscale-parameters-explained/
+http://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
+http://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
--- main.py
+++ main.py
@@ -12,19 +12,16 @@ import sys
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-imagePath = "images/jake.jpg"
+imagePath = "../images/jake.jpg"
 # load the image and resize it to (1) reduce detection time
 # and (2) improve detection accuracy
-
-
 
 video_capture = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    ret, image = video_capture.read()
 
-    image = frame #cv2.imread(imagePath)
-    image = imutils.resize(image, width=min(800, image.shape[1]))
+    image = imutils.resize(image, width=min(300, image.shape[1]))
     orig = image.copy()
 
     # detect people in the image
@@ -45,22 +42,15 @@ while True:
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-    # show some information on the number of bounding boxes
 
-    # filename = imagePath[imagePath.rfind("/") + 1:]
+    print(" {} original boxes, {} after suppression".format(len(rects), len(pick)))
 
-    # print("[INFO] {}: {} original boxes, {} after suppression".format(
-    #     filename, len(rects), len(pick)))
-        # Display the resulting frame
+
     cv2.imshow('Video', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+    # if cv2.waitKey(1) & 0xFF == 113: # This is q
+    #     break
 
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
-
-# show the output images
-# cv2.imshow("Before NMS", orig)
-# cv2.imshow("After NMS", image)
-# cv2.waitKey(0)
