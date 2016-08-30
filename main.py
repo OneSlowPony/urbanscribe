361979cbd4435a60697276c7d3895b475a71a899
Jake Coppinger <jake@jakecoppinger.com>
2016-08-31 09:46:29 +1000

    Recognise faces and people

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ import sys
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-imagePath = "../images/jake.jpg"
-# load the image and resize it to (1) reduce detection time
-# and (2) improve detection accuracy
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 while True:
@@ -25,25 +24,37 @@ while True:
     orig = image.copy()
 
     # detect people in the image
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+    (people, weights) = hog.detectMultiScale(image, winStride=(4, 4),
         padding=(8, 8), scale=1.05)
 
+    # Detect faces
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    objects = faces
+
     # draw the original bounding boxes
-    for (x, y, w, h) in rects:
+    for (x, y, w, h) in objects:
         cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    objects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in objects])
+    pick = non_max_suppression(objects, probs=None, overlapThresh=0.65)
 
     # draw the final bounding boxes
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
 
-    print(" {} original boxes, {} after suppression".format(len(rects), len(pick)))
+    print(" {} original boxes, {} after suppression".format(len(objects), len(pick)))
 
 
     cv2.imshow('Video', image)
