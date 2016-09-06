12c5e6e34f52f457d732ca0470ba722d9ea44326
Jake Coppinger <jake@jakecoppinger.com>
2016-09-06 12:47:41 +1000

    Fast capture stream

--- main.py
+++ main.py
@@ -12,15 +12,19 @@ import sys
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+imagePath = "images/jake.jpg"
+# load the image and resize it to (1) reduce detection time
+# and (2) improve detection accuracy
+
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
-    ret, image = video_capture.read()
+    ret, frame = video_capture.read()
 
-    image = imutils.resize(image, width=min(300, image.shape[1]))
+    image = imutils.resize(frame, width=min(300, frame.shape[1]))
     orig = image.copy()
 
     # detect people in the image
@@ -28,40 +32,47 @@ while True:
         padding=(8, 8), scale=1.05)
 
     # Detect faces
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # faces = faceCascade.detectMultiScale(
-    #     gray,
-    #     scaleFactor=1.1,
-    #     minNeighbors=5,
-    #     minSize=(30, 30),
-    #     flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    # )
 
-    objects = people
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    rects = faces
 
     # draw the original bounding boxes
-    for (x, y, w, h) in objects:
+    for (x, y, w, h) in rects:
         cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
-    objects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in objects])
-    pick = non_max_suppression(objects, probs=None, overlapThresh=0.65)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
     # draw the final bounding boxes
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
+    # show some information on the number of bounding boxes
 
-    print(" {} original boxes, {} after suppression".format(len(objects), len(pick)))
+    # filename = imagePath[imagePath.rfind("/") + 1:]
 
-
-    cv2.imshow('Video', image)
-
-    # if cv2.waitKey(1) & 0xFF == 113: # This is q
-    #     break
+    print("{} original boxes, {} after suppression".format(len(rects), len(pick)))
+        # Display the resulting frame
+    #cv2.imshow('Video', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything is done, release the capture
 video_capture.release()
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
+
+# show the output images
+# cv2.imshow("Before NMS", orig)
+# cv2.imshow("After NMS", image)
+# cv2.waitKey(0)
