b3dc2c61898fb5fdee9ebe6393fa8e5b5033507a
Jake Coppinger <jake@jakecoppinger.com>
2016-09-06 13:37:14 +1000

    Added basic install instructions for linux

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt-get install build-essential git cmake pkg-config
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python2.7-dev python3-dev
+sudo apt-get install python-opencv
+
+
+sudo pip install imutils numpy
