92c6470cbdb47f59912917aa27574d5ca5c76744
Jake Coppinger <jake@jakecoppinger.com>
2016-10-23 12:48:09 +1100

    Setup image recognition for people

--- image_recognition.py
+++ image_recognition.py
@@ -8,13 +8,13 @@ import json
 import math
 import cv2
 
-# hog = cv2.HOGDescriptor()
-# hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 aspectRatio = float(16)/9
-imageWidth = 500
+imageWidth = 300
 imageHeight = imageWidth / aspectRatio
 
 
@@ -26,22 +26,20 @@ def recognisePeople(image):
     image = cv2.flip(image,1)
 
     # Comment here
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    rects = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # rects = faceCascade.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30),
+    #     flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    # )
 
     # Or here!
-    # (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-    #                                             padding=(8, 8), scale=1.05)
-
-    # draw the original bounding boxes
-    # for (x, y, w, h) in rects:
-    #     cv2.rectangle(imageCopy, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+                                                padding=(8, 8), scale=1.05)
+    for (x, y, w, h) in rects:
+        cv2.rectangle(imageCopy, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
